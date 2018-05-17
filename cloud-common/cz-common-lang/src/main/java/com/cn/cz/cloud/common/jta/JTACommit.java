a583c050761f647a2ea65bbfc4f5331612554cf1
ywaz <1411033077@qq.com>
2018-05-17 10:18:17 +0800

    jta

--- cloud-common/cz-common-lang/src/main/java/com/cn/cz/cloud/common/jta/JTACommit.java
+++ cloud-common/cz-common-lang/src/main/java/com/cn/cz/cloud/common/jta/JTACommit.java
@@ -4,11 +4,8 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
-import javax.transaction.UserTransaction;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.transaction.*;
+import java.sql.*;
 import java.util.Properties;
 
 /**
@@ -17,7 +14,7 @@ import java.util.Properties;
  */
 public class JTACommit {
 
-    public static void main (String[] args) throws NamingException, SQLException {
+    public static void main (String[] args) throws NamingException, SQLException, HeuristicRollbackException, RollbackException, HeuristicMixedException, SystemException, NotSupportedException {
         Context ctx = new InitialContext();
         // Change these two lines to suit your environment.
         DataSource ds = (DataSource)ctx.lookup("jdbc/ExampleDS");
@@ -28,100 +25,111 @@ public class JTACommit {
 
         // Get a UserTransaction
         UserTransaction txn = (UserTransaction) new InitialContext().lookup("java:comp/UserTransaction");
+        txn.begin();
+        Connection mySqlConnection = DriverManager.getConnection("localhost:3306","root","123456");
 
-        try {
-            stmt = conn.createStatement();  // non-tx statement
+        Statement mySqlStat = mySqlConnection.createStatement();
+        mySqlStat.execute("update t_account set amount = amount - 500 where account_id = 'A'");
 
-            // Check the database connection.
-            try {
-                stmt.executeUpdate("DROP TABLE test_table");
-                stmt.executeUpdate("DROP TABLE test_table2");
-            }
-            catch (Exception e) {
-                // assume not in database.
-            }
+        Connection mySqlConnection2 = DriverManager.getConnection("192.168.1.2","root","123456");
+        Statement mySqlStat2 = mySqlConnection2.createStatement();
+        mySqlStat2.execute("update t_account set amount = amount - 500 where account_id = 'A'");
 
-            try {
-                stmt.executeUpdate("CREATE TABLE test_table (a INTEGER,b INTEGER)");
-                stmt.executeUpdate("CREATE TABLE test_table2 (a INTEGER,b INTEGER)");
-            }
-            catch (Exception e) {
-            }
+        txn.commit();
 
-            try {
-                System.out.println("Starting top-level transaction.");
-
-                txn.begin();
-
-                stmtx = conn.createStatement(); // will be a tx-statement
-
-                // First, we try to roll back changes
-
-                System.out.println("\nAdding entries to table 1.");
-
-                stmtx.executeUpdate("INSERT INTO test_table (a, b) VALUES (1,2)");
-
-                ResultSet res1 = null;
-
-                System.out.println("\nInspecting table 1.");
-
-                res1 = stmtx.executeQuery("SELECT * FROM test_table");
-
-                while (res1.next()) {
-                    System.out.println("Column 1: "+res1.getInt(1));
-                    System.out.println("Column 2: "+res1.getInt(2));
-                }
-                System.out.println("\nAdding entries to table 2.");
-
-                stmtx.executeUpdate("INSERT INTO test_table2 (a, b) VALUES (3,4)");
-                res1 = stmtx.executeQuery("SELECT * FROM test_table2");
-
-                System.out.println("\nInspecting table 2.");
-
-                while (res1.next()) {
-                    System.out.println("Column 1: "+res1.getInt(1));
-                    System.out.println("Column 2: "+res1.getInt(2));
-                }
-
-                System.out.print("\nNow attempting to rollback changes.");
-
-                txn.rollback();
-
-                // Next, we try to commit changes
-                txn.begin();
-                stmtx = conn.createStatement();
-                ResultSet res2 = null;
-
-                System.out.println("\nNow checking state of table 1.");
-
-                res2 = stmtx.executeQuery("SELECT * FROM test_table");
-
-                while (res2.next()) {
-                    System.out.println("Column 1: "+res2.getInt(1));
-                    System.out.println("Column 2: "+res2.getInt(2));
-                }
-
-                System.out.println("\nNow checking state of table 2.");
-
-                stmtx = conn.createStatement();
-
-                res2 = stmtx.executeQuery("SELECT * FROM test_table2");
-
-                while (res2.next()) {
-                    System.out.println("Column 1: "+res2.getInt(1));
-                    System.out.println("Column 2: "+res2.getInt(2));
-                }
-
-                txn.commit();
-            }
-            catch (Exception ex) {
-                ex.printStackTrace();
-                System.exit(0);
-            }
-        }
-        catch (Exception sysEx) {
-            sysEx.printStackTrace();
-            System.exit(0);
-        }
+//        try {
+//            stmt = conn.createStatement();  // non-tx statement
+//
+//            // Check the database connection.
+//            try {
+//                stmt.executeUpdate("DROP TABLE test_table");
+//                stmt.executeUpdate("DROP TABLE test_table2");
+//            }
+//            catch (Exception e) {
+//                // assume not in database.
+//            }
+//
+//            try {
+//                stmt.executeUpdate("CREATE TABLE test_table (a INTEGER,b INTEGER)");
+//                stmt.executeUpdate("CREATE TABLE test_table2 (a INTEGER,b INTEGER)");
+//            }
+//            catch (Exception e) {
+//            }
+//
+//            try {
+//                System.out.println("Starting top-level transaction.");
+//
+//                txn.begin();
+//
+//                stmtx = conn.createStatement(); // will be a tx-statement
+//
+//                // First, we try to roll back changes
+//
+//                System.out.println("\nAdding entries to table 1.");
+//
+//                stmtx.executeUpdate("INSERT INTO test_table (a, b) VALUES (1,2)");
+//
+//                ResultSet res1 = null;
+//
+//                System.out.println("\nInspecting table 1.");
+//
+//                res1 = stmtx.executeQuery("SELECT * FROM test_table");
+//
+//                while (res1.next()) {
+//                    System.out.println("Column 1: "+res1.getInt(1));
+//                    System.out.println("Column 2: "+res1.getInt(2));
+//                }
+//                System.out.println("\nAdding entries to table 2.");
+//
+//                stmtx.executeUpdate("INSERT INTO test_table2 (a, b) VALUES (3,4)");
+//                res1 = stmtx.executeQuery("SELECT * FROM test_table2");
+//
+//                System.out.println("\nInspecting table 2.");
+//
+//                while (res1.next()) {
+//                    System.out.println("Column 1: "+res1.getInt(1));
+//                    System.out.println("Column 2: "+res1.getInt(2));
+//                }
+//
+//                System.out.print("\nNow attempting to rollback changes.");
+//
+//                txn.rollback();
+//
+//                // Next, we try to commit changes
+//                txn.begin();
+//                stmtx = conn.createStatement();
+//                ResultSet res2 = null;
+//
+//                System.out.println("\nNow checking state of table 1.");
+//
+//                res2 = stmtx.executeQuery("SELECT * FROM test_table");
+//
+//                while (res2.next()) {
+//                    System.out.println("Column 1: "+res2.getInt(1));
+//                    System.out.println("Column 2: "+res2.getInt(2));
+//                }
+//
+//                System.out.println("\nNow checking state of table 2.");
+//
+//                stmtx = conn.createStatement();
+//
+//                res2 = stmtx.executeQuery("SELECT * FROM test_table2");
+//
+//                while (res2.next()) {
+//                    System.out.println("Column 1: "+res2.getInt(1));
+//                    System.out.println("Column 2: "+res2.getInt(2));
+//                }
+//
+//                txn.commit();
+//            }
+//            catch (Exception ex) {
+//                ex.printStackTrace();
+//                System.exit(0);
+//            }
+//        }
+//        catch (Exception sysEx) {
+//            sysEx.printStackTrace();
+//            System.exit(0);
+//        }
     }
 }
